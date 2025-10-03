7e5f004ab466392993fc1be756c0d92a3e54f7ce
HoDucDuy2003 <duy123pd11@gmail.com>
2025-10-03 11:43:31 +0700

    test

---
+++ Two Pointers/Container With Most Water.cs
@@ -0,0 +1,25 @@
+public class Solution
+{
+    public int MaxArea(int[] height)
+    {
+        int L = 0;
+        int R = height.Length - 1;
+
+        int res = 0;
+
+        while (L < R)
+        {
+            res = Math.Max(res, (R - L) * Math.Min(height[L], height[R]));
+
+            if (height[L] < height[R])
+            {
+                L++;
+            }
+            else
+            {
+                R--;
+            }
+        }
+        return res;
+    }
+}
